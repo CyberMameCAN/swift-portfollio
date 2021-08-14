c7d2aa2bdff6417ed1d6c6ef1a17a256f9865354
Toshiya Takeshita <toshi@atbat.jp>
2021-08-14 09:58:38 +0900

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by としや on 2021/08/14.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
